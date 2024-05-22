fae66da541f108536453808bc80d932dbaf0dfaf
MataclysmGames <matterickson915@gmail.com>
2024-05-22 16:34:10 -0700

    remove fullscreen button

--- scripts/title.gd
+++ scripts/title.gd
@@ -3,14 +3,12 @@ extends CanvasLayer
 @onready var create_character_button: Button = $MarginContainer/VBoxContainer/VBoxContainer/CreateCharacterButton
 @onready var play_pazaak_button: Button = $MarginContainer/VBoxContainer/VBoxContainer/PlayPazaakButton
 @onready var exit_button: Button = $MarginContainer/VBoxContainer/VBoxContainer/ExitButton
-@onready var check_box: CheckBox = $MarginContainer/VBoxContainer/HBoxContainer/CheckBox
 
 func _ready() -> void:
 	DisplayServer.screen_set_orientation(DisplayServer.SCREEN_LANDSCAPE)
 	create_character_button.pressed.connect(create_character)
 	play_pazaak_button.pressed.connect(play_pazaak)
 	exit_button.pressed.connect(exit)
-	check_box.toggled.connect(on_check_box)
 	
 func create_character():
 	get_tree().change_scene_to_file("res://scenes/character_creation.tscn")
@@ -20,11 +18,3 @@ func play_pazaak():
 
 func exit():
 	get_tree().quit()
-
-func on_check_box(toggled_on : bool):
-	if toggled_on:
-		DisplayServer.screen_set_orientation(DisplayServer.SCREEN_LANDSCAPE)
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-	else:
-		DisplayServer.screen_set_orientation(DisplayServer.SCREEN_LANDSCAPE)
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
