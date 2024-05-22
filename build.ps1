ec35da7d3efc7f950fbdc66bfb9db8084487fbca
MataclysmGames <matterickson915@gmail.com>
2024-05-22 12:50:42 -0700

    godot 4.3

--- build.ps1
+++ build.ps1
@@ -2,6 +2,8 @@ godot_console.exe --headless --export-release "Windows Desktop" --quiet
 godot_console.exe --headless --export-release "Linux" --quiet
 godot_console.exe --headless --export-release "Web" --quiet
 
+cp -Force C:\Users\matte\Documents\Games\Star-Wars-RPG-Tools\Web\* C:\Users\matte\Documents\GitHub\mataclysmgames.github.io\star-wars-rpg-tools\
+
 butler push C:\Users\matte\Documents\Games\Star-Wars-RPG-Tools\Linux\ mataclysm/Star-Wars-RPG-Tools:linux
 butler push C:\Users\matte\Documents\Games\Star-Wars-RPG-Tools\Windows\ mataclysm/Star-Wars-RPG-Tools:windows
 butler push C:\Users\matte\Documents\Games\Star-Wars-RPG-Tools\Web\ mataclysm/Star-Wars-RPG-Tools:html
